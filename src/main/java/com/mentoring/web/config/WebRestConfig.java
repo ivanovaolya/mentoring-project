9c6c602d029e93479c78f7b753dd626998ef315a
ivanovaolya <olya.ivanova.kiev@gmail.com>
2017-07-14 17:00:59 +0300

    Added JSON modelMapper instead of custom one

---
+++ src/main/java/com/mentoring/web/config/WebRestConfig.java
@@ -0,0 +1,26 @@
+package com.mentoring.web.config;
+
+import com.mentoring.web.converter.Converter;
+import com.mentoring.web.converter.UserConverter;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ivanovaolyaa
+ * @version 7/14/2017
+ */
+@Configuration
+public class WebRestConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public Converter userConverter() {
+        return new UserConverter();
+    }
+}
---
+++ src/main/java/com/mentoring/web/converter/Converter.java
@@ -0,0 +1,13 @@
+package com.mentoring.web.converter;
+
+/**
+ * @author ivanovaolyaa
+ * @version 7/14/2017
+ */
+public interface Converter<T, V> {
+
+    T convertToEntity(V dto);
+
+    V convertToDto(T entity);
+
+}
