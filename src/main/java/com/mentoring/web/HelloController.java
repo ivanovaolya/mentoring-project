d12474cc3407864607dcee118139819ddaaf997a
ivanovaolya <olya.ivanova.kiev@gmail.com>
2017-07-14 15:49:41 +0300

    Deleted Hello-world samples

--- src/main/java/com/mentoring/web/HelloController.java
+++
@@ -1,5 +0,0 @@
-Unstaged changes after reset:
-M	build.gradle
-M	src/main/java/com/mentoring/Application.java
-D	src/main/java/com/mentoring/controller/HelloController.java
-M	src/main/java/com/mentoring/web/HelloController.java
