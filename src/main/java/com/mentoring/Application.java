a02a78897ecad8b18a2e8b9c0d20e5d931a5b072
ivanovaolya <olya.ivanova.kiev@gmail.com>
2017-07-14 15:41:31 +0300

    Added additional configs for spring and usefull libraries

--- src/main/java/com/mentoring/Application.java
+++ src/main/java/com/mentoring/Application.java
@@ -3,7 +3,7 @@ package com.mentoring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class Application {
 
     public static void main(String[] args) {
