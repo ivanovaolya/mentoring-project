b1ebeb788c1bca2b200df79ac4286986cf13d231
ivanovaolya <olya.ivanova.kiev@gmail.com>
2017-09-18 18:36:37 +0300

    Added default users for ADMIN and USER groups

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,7 @@
 DROP TABLE IF EXISTS roles;
 INSERT INTO roles VALUES (1, 'ROLE_USER'),(2, 'ROLE_ADMIN');
+
+INSERT INTO users (user_pk, email, password) VALUES (1, 'admin@gmail.com', 'admin1234');
+INSERT INTO user_role (User_user_pk, roles_role_pk) VALUES (1, 2);
+INSERT INTO users (user_pk, email, password) VALUES (2, 'user@gmail.com', 'user1234');
+INSERT INTO user_role (User_user_pk, roles_role_pk) VALUES (2, 1);
