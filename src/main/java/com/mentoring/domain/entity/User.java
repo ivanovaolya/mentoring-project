7ad4d2b80604b0bfcc3e26319ec58b31feb81c06
ivanovaolya <olya.ivanova.kiev@gmail.com>
2017-09-19 13:34:06 +0300

    Changed security configuration

--- src/main/java/com/mentoring/domain/entity/User.java
+++ src/main/java/com/mentoring/domain/entity/User.java
@@ -22,7 +22,7 @@ import java.util.Set;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_pk")
     private Long userPk;
 
